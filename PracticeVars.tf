e95031405f212c80821d92dedc5a94951bf30d9c
Srivalli Vadlamani <vallinookal@gmail.com>
2024-04-13 16:59:19 +0530

    first var commit

---
+++ PracticeVars.tf
@@ -0,0 +1,27 @@
+provider "aws"{
+    region = "ap-south-1"
+    access_key = var.access_key
+    secret_key = var.secret_key
+}
+
+
+
+ resource "aws_instance" "Arjun" {
+    ami = "ami-05a5bb48beb785bf1"
+    instance_type = var.cputype
+    key_name = "red hat"
+     tags = {
+      Name = "Terraform"
+   }
+   availability_zone = "ap-south-1b"
+}
+
+resource "aws_instance" "Mani" {
+    ami = "ami-05a5bb48beb785bf1"
+    instance_type = var.cputype
+    key_name = "red hat"
+    tags = {
+        Name = "Arjun"
+    }
+  
+}
---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "cputype" {
+  default = "t2.small"
+}
+
+variable "access_key"{
+  default =   "AKIATCKAR36FNEFNRONR"
+}
+
+variable "secret_key" {
+  default = "wVUeWVsdylxc/A1sgUx7vKr+a0hKmgC9Tl3hPO9f"
+  }
+
